bbaf8b7a3e149415aaf36bd8d2c430934f06fe57
sanny.do <sanny.do94@gmail.com>
2020-10-22 14:22:56 -0500

    stuff and things'

--- lib/app.rb
+++ lib/app.rb
@@ -15,7 +15,7 @@ class Prompt
 
     def main_menu
         main_menu_options =["Go to Favorites", "Find where to Watch Favorites"]
-        user = @@prompt.select("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHi, welcome to the main menu. Please select from the following list below:", main_menu_options)
+        @@prompt.select("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nHi, welcome to the main menu. Please select from the following list below:", main_menu_options)
     end
 
     def people_or_movie
@@ -34,24 +34,25 @@ class Prompt
                 name: item.name,
                 id: item.imdb_id
             }
-            end
-            temp_array << "Exit"
-            temp_array.unshift("Add to your Favorites")
+        end
+        temp_array << "Exit"
+        temp_array.unshift("Add to your Favorites")
         #binding.pry
         value = @@prompt.select("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nChoose a favorite to remove it or exit to go back", temp_array)
-            if value == "Add to your Favorites"
-                choice1 = people_or_movie()
-                if choice1 == "Actor/Actress"
-                    puts "Enter name of Actress/Actor (full name preferred)"
-                    name = STDIN.gets.chomp()
-                    list = api.search_people(name)
-                    choice2 = @@prompt.select("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nChoose from the list of names", list[:name] )
-                    choice2
+        if value == "Add to your Favorites"
+            choice1 = people_or_movie()
+            if choice1 == "Actor/Actress"
+                puts "Enter name of Actress/Actor (full name preferred)"
+                name = STDIN.gets.chomp()
+                list = api.search_people(name)
+                choice2 = @@prompt.select("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\nChoose from the list of names", list[:name] )
+                choice2
 
-                elsif choice1 == "Movies"
-                
-                end
+            elsif choice1 == "Movies"
+            
             end
+        end
+       
     end
 
     def name_of_movie
--- lib/run.rb
+++ lib/run.rb
@@ -1,12 +1,13 @@
-# require_relative "../config/environment.rb"
+require_relative "../config/environment.rb"
 
-# TERMINAL = Prompt.new()
+TERMINAL = Prompt.new()
 
-# TERMINAL.welcome_user()
-# choice1 = TERMINAL.main_menu()
-# if choice1 == "Go To Favorites"
-#   choice2 = TERMINAL.favorite_menu()
-#   if choice2
-# elsif  choice1 == "Find where to Watch Favorites"
-#   puts "other"
-# end
+TERMINAL.welcome_user()
+choice1 = TERMINAL.main_menu()
+
+binding.pry
+if choice1 == "Go to Favorites"
+  choice2 = TERMINAL.favorite_menu()
+elsif  choice1 == "Find where to Watch Favorites"
+  puts "other"
+end
